d707e29b5595bce32cff7da20a092389f494d9ee
KOHEI183 <zbtbfuufp@gmail.com>
2021-09-16 17:58:10 +0900

    フォームの追加 #20

---
+++ src/components/Molecules/AppCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <VCard
+    :class="className"
+  >
+    <v-container>
+      <v-col>
+        <slot/>
+      </v-col>
+    </v-container>
+  </VCard>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import VCard from '@/components/Atoms/Card/VCard.vue'
+
+export default Vue.extend({
+  components: {
+    VCard
+  },
+  props: {
+    className: {
+      type: String,
+      default: ''
+    }
+  }
+})
+</script>
